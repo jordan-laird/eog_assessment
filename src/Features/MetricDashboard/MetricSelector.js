5cc0ae4298a8489bccc5fa9e203b08e06ddbc366
jordan-laird <jordanmack.laird@gmail.com>
2019-12-16 15:01:35 -0600

    .5 hrs - wrapped Select component in FormControl, added InputLabel, and styled components #6

--- src/Features/MetricDashboard/MetricSelector.js
+++ src/Features/MetricDashboard/MetricSelector.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actions } from './reducer';
-import { Select, MenuItem, Chip } from '@material-ui/core';
+import { Select, MenuItem, Chip, FormControl, InputLabel } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  metricSelector:{
+    minWidth: 150,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  selectedChip:{
+    marginLeft: 2,
+    marginRight: 2
+  }
+})
 
 
 const MetricSelector = () => {
+  const classes = useStyles()
   const dispatch = useDispatch();
   const availableMetrics = useSelector(state => state.metrics.availableMetrics);
   const selectedMetrics = useSelector(state => state.metrics.selectedMetrics);
@@ -21,23 +35,27 @@ const MetricSelector = () => {
 
   return (
     <div>
-      <Select
-        value={selectedMetrics}
-        onChange={selectMetric}
-        renderValue={selected => (
-          <div>
-            {selected.map(value => (
-              <Chip value={value} key={`${value}:selected`}onDelete={() => deselectMetric(value)} label={value} />
-            ))}
-          </div>
-        )}
-      >
-        {metricsForSelectDropdown.map(metric => (
-          <MenuItem key={metric} value={metric}>
-            {metric}
-          </MenuItem>
-        ))}
-      </Select>
+      <FormControl className={classes.metricSelector}>
+        <InputLabel id="metricSelect">Select Metric</InputLabel>
+        <Select
+          labelId="metricSelect"
+          value={selectedMetrics}
+          onChange={selectMetric}
+          renderValue={selected => (
+            <div>
+              {selected.map(value => (
+                <Chip className={classes.selectedChip} value={value} key={`${value}:selected`} onDelete={() => deselectMetric(value)} label={value} />
+              ))}
+            </div>
+          )}
+        >
+          {metricsForSelectDropdown.map(metric => (
+            <MenuItem key={metric} value={metric}>
+              {metric}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 }
